42164ade4ee5bd8777aa29dd5fd80b78a46aa5c8
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-04-17 06:08:08 -0600

    Implement movie data model
    implemented constructors and getters methods
    
    They will be filled out with real data from the internet

--- app/src/main/java/com/example/android/popularmovies/model/Movies.java
+++ app/src/main/java/com/example/android/popularmovies/model/Movies.java
@@ -1,4 +1,46 @@
 package com.example.android.popularmovies.model;
 
 public class Movies {
+
+    private String mTitle;
+    private String mThumbnail;
+    private String mOverView;
+    private String mUserRatings;
+    private String mReleaseDate;
+
+    /**
+     * No args constructor for use in serialization
+     */
+    public Movies() {
+    }
+
+    public Movies (String title, String thumbnail, String overView, String ratings, String releaseDate){
+        mTitle = title;
+        mThumbnail = thumbnail;
+        mOverView = overView;
+        mUserRatings = ratings;
+        mReleaseDate = releaseDate;
+    }
+
+    String getTitle(){
+        return mTitle;
+    }
+
+    String getThumbnail(){
+        return mThumbnail;
+    }
+
+    String getOverView(){
+        return mOverView;
+    }
+
+    String getUserRatings(){
+        return mUserRatings;
+    }
+
+    String getReleaseDate(){
+        return mReleaseDate;
+    }
+
+
 }
