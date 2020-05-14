1c8cb918797f3d3ec524d197e7b6f6e3990b2c89
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-05-14 01:47:07 -0600

    Broken Shared Preference
    I changed my changed preference from porpularity.desc(default) and poprpularity.asc to popular and top_rated. However, on app install, the default shared preference is still porpularity.desc.

--- app/src/main/java/com/example/android/popularmovies/sync/MovieSyncTask.java
+++ app/src/main/java/com/example/android/popularmovies/sync/MovieSyncTask.java
@@ -3,11 +3,13 @@ package com.example.android.popularmovies.sync;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.repositiory.MovieRepository;
 
 public class MovieSyncTask {
+    private static final String LOG_TAG = MovieSyncTask.class.getSimpleName();
 
     synchronized public static void syncMovie(Context context) {
 
@@ -20,8 +22,11 @@ public class MovieSyncTask {
         //get the prefered sort value
         String sortBy  = sharedPrefs.getString(
                 context.getString(R.string.settings_sort_key),
-                context.getString(R.string.settings_sort_default_value)
+                context.getString(R.string.pref_sort_default_value)
         );
+
+        Log.d(LOG_TAG, "SortMoviedefault: " + sortBy); //it still shows popularity.desc on app install???
+
 
         return sortBy;
 
